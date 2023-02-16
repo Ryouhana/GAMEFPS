675930f7481d0cf9482e3141be3bfdf799e4667b
hanaryou0730 <hanaryo730@gmail.com>
2023-02-17 04:18:39 +0900

    武器の切り替え機能

--- EmptyDxGame/program/game/model/weapon.cpp
+++ EmptyDxGame/program/game/model/weapon.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 Weapon::Weapon() {
-	HandGunImage = LoadGraph("graphics/Handgun.png");
+	hand_status.HandGunImage = LoadGraph("graphics/Handgun.png");
+	sub_status.SubMachineGunImage = LoadGraph("graphics/MP5.png");
+	assault_status.AssaultRifleImage = LoadGraph("graphics/AK-47.png");
 	Tama_0 = LoadSoundMem("sound/Gun_SE/no.mp3");
 	HandGun_Reroad = LoadSoundMem("sound/Gun_SE/HandgunReroad.mp3");
 	debug = LoadGraph("graphics/Ring.png");
@@ -17,68 +19,150 @@ void Weapon::Update(float deltaTime) {
 	int wheelMax = 3;
 	int wheelMin = 1;
 	mousewheel += GetMouseWheelRotVol();
-	if (GunReroad) {
-		debug_i += 2.0f;
+	
+	if (mousewheel > wheelMax) {
+		mousewheel = 1;
+	}
+	if (mousewheel < wheelMin) {
+		mousewheel = 3;
+	}
+	/*if (mousewheel == HandGun) {
+		HandGunFlag = true;
+		SwitchWeapon(HandGun);
+	}*/
+	/*else HandGunFlag = false;*/
+	if (mousewheel == SubMachineGun) {
+		if (!SubMachineGunFlah) {
+			reroadtime = sub_status.reroadtime;
+			ammunition = sub_status.ammunition;
+			maxAmmunition = sub_status.maxAmmunition;
+			ammoClip = sub_status.ammoClip;
+			maxAmmoClip = sub_status.maxAmmoClip;
+			amountNeed = sub_status.amountNeed;
+			ammoAvailable = sub_status.ammoAvailable;
+			SubMachineGunFlah = true;
+		}
 
-		if (debug_i >= 100) {
+		SwitchWeapon(SubMachineGun);
+	}
+	else SubMachineGunFlah = false;
+	if (mousewheel == AssaultRifle) {
+		if (!AssaultRifleFlag) {
+
+			reroadtime = assault_status.reroadtime;
+			ammunition = assault_status.ammunition;
+			maxAmmunition = assault_status.maxAmmunition;
+			ammoClip = assault_status.ammoClip;
+			maxAmmoClip = assault_status.maxAmmoClip;
+			amountNeed = assault_status.amountNeed;
+			ammoAvailable = assault_status.ammoAvailable;
+			AssaultRifleFlag = true;
+		}
+		SwitchWeapon(AssaultRifle);
+	}
+	else AssaultRifleFlag = false;
+	if (mousewheel == HandGun) {
+		if (!HandGunFlag) {
+
+			reroadtime = hand_status.reroadtime;
+			ammunition = hand_status.ammunition;
+			maxAmmunition = hand_status.maxAmmunition;
+			ammoClip = hand_status.ammoClip;
+			maxAmmoClip = hand_status.maxAmmoClip;
+			amountNeed = hand_status.amountNeed;
+			ammoAvailable = hand_status.ammoAvailable;
+			HandGunFlag = true;
+		}
+		SwitchWeapon(HandGun);
+	}
+	else HandGunFlag = false;
+	if (GunReroad) {
+		normalreroad_time += reroadtime;
+
+		if (normalreroad_time >= 100) {
 			ammunition -= ammoAvailable;
 			ammoClip += ammoAvailable;
-			debug_i = 0;
+			normalreroad_time = 0;
 			GunReroad = false;
 		}
 
 	}
-		if (mousewheel > wheelMax) {
-			mousewheel = 1;
-		}
-		if (mousewheel < wheelMin) {
-			mousewheel = 3;
-		}
-		if (mousewheel == wheelMin) {
-			HandGunFlag = true;
-		}
-		else HandGunFlag = false;
-		if (W_type == HandGun) {
-			if (tnl::Input::IsMouseTrigger(tnl::Input::eMouseTrigger::IN_LEFT) && !GunReroad) {
-				if (ammoClip > 0) {
-					PlaySoundMem(mgr->juusei, DX_PLAYTYPE_BACK);
+		if (tnl::Input::IsMouseTrigger(tnl::Input::eMouseTrigger::IN_LEFT) && !GunReroad) {
+			if (ammoClip > 0) {
+				PlaySoundMem(mgr->juusei, DX_PLAYTYPE_BACK);
 
-					ammoClip--;
-				}
-
-				if (ammoClip <= 0) {
-					ammoClip = 0;
-					PlaySoundMem(Tama_0, DX_PLAYTYPE_BACK);
-				}
+				ammoClip--;
 			}
 
-			if (tnl::Input::IsKeyDownTrigger(eKeys::KB_R)) {
-				amountNeed = maxAmmoClip - ammoClip;
-				//ƒ}ƒKƒWƒ“‚É•â[‚·‚é‚×‚«’e–ò”‚æ‚èŠŽ‚µ‚Ä‚é’e–ò”‚ª‘å‚«‚¢‚©
-				ammoAvailable = amountNeed < ammunition ? amountNeed : ammunition;
-				if (amountNeed != 0 && ammunition != 0) {
-					if (!GunReroad)PlaySoundMem(HandGun_Reroad, DX_PLAYTYPE_BACK);
-					GunReroad = true;
-				}
-
-
+			if (ammoClip <= 0) {
+				ammoClip = 0;
+				PlaySoundMem(Tama_0, DX_PLAYTYPE_BACK);
 			}
+		}
+
+		if (tnl::Input::IsKeyDownTrigger(eKeys::KB_R)) {
+			amountNeed = maxAmmoClip - ammoClip;
+			//ƒ}ƒKƒWƒ“‚É•â[‚·‚é‚×‚«’e–ò”‚æ‚èŠŽ‚µ‚Ä‚é’e–ò”‚ª‘å‚«‚¢‚©
+			ammoAvailable = amountNeed < ammunition ? amountNeed : ammunition;
+			if (amountNeed != 0 && ammunition != 0) {
+				if (!GunReroad)PlaySoundMem(HandGun_Reroad, DX_PLAYTYPE_BACK);
+				GunReroad = true;
+			}
+
+
+		}
+	
+	
+	
+
 		
-		}
 }
 
 void Weapon::Render() {
 	SetFontSize(55);
 	//•¶Žš‚Ì•`‰æ
-	if (HandGunFlag) {
+	//if (HandGunFlag) {
 		DrawStringEx(840, 680, -1, "%2d", ammoClip);
 		SetFontSize(40);
 		DrawStringEx(895, 720, -1, "/%d", ammunition);
 
-		DrawRotaGraph(775, 720, 0.15f, 0, HandGunImage, true);
-		DrawCircleGauge(reroad_x + 5, reroad_y + 8.5f, debug_i, debug, 0.0f, 0.08f);
-	}
+		DrawRotaGraph(775, 720, 0.15f, 0, NormalWeaponImage, true);
+		DrawCircleGauge(reroad_x + 5, reroad_y + 8.5f, normalreroad_time, debug, 0.0f, 0.08f);
+	//}
 	DrawStringEx(840, 600, -1, "%d", mousewheel);
+}
+
+void Weapon::SwitchWeapon(WeaponType weapontype) {
+	switch (weapontype) {
+	case WeaponType::AssaultRifle:
+		
+		NormalWeaponImage = assault_status.AssaultRifleImage;
+		NormalWeapon_Reroad = assault_status.AssaultRifle_Reroad;
+		
+		reroad_x = assault_status.reroad_x;
+		reroad_y = assault_status.reroad_y;
+		break;
+	case WeaponType::HandGun:
+		
+		NormalWeaponImage = hand_status.HandGunImage;
+		NormalWeapon_Reroad = hand_status.HandGun_Reroad;
+		
+		
+		reroad_x = hand_status.reroad_x;
+		reroad_y = hand_status.reroad_y;
+		break;
+	case WeaponType::SubMachineGun:
+	
+		NormalWeaponImage = sub_status.SubMachineGunImage;
+		NormalWeapon_Reroad = sub_status.SubMachinGun_Rrroad;
+		
+		reroad_x = sub_status.reroad_x;
+		reroad_y = sub_status.reroad_y;
+		break;
+	default:
+		return;
+
+	 }
 }
 //•Ší‚Ì•`‰æ‚ª‚Å‚«‚Ä‚©‚ç
 //void Weapon::Render(Player *p, int weapon, int type) {
--- EmptyDxGame/program/game/model/weapon.h
+++ EmptyDxGame/program/game/model/weapon.h
@@ -11,14 +11,58 @@ public:
 		AssaultRifle
 
 	};
-	WeaponType W_type;
+
+	
 	Weapon();
 	~Weapon() {};
+	struct HandGunStatus {
+		int HandGunImage = 0;
+		//Š’e–òAÅ‚Š’e–òAƒ}ƒKƒWƒ““à‚Ì’e”Aƒ}ƒKƒWƒ““à‚ÌÅ‘å”
+		int ammunition = 60, maxAmmunition = 60, ammoClip = 15, maxAmmoClip = 15;
+		//•â[‚·‚é‚×‚«’e–ò‚Ì•Ï”‚Æ•â[‚·‚é’e–ò‚Ì•Ï”
+		int amountNeed = 0, ammoAvailable = 0;
+		float reroad_x = 500.0, reroad_y = 400.0;
+		float reroadtime = 2.0f;
+		int HandGun_Reroad = 0;
+	};
+	HandGunStatus hand_status;
+
+	struct SubMachineGunStatus {
+		int SubMachineGunImage = 0;
+		//Š’e–òAÅ‚Š’e–òAƒ}ƒKƒWƒ““à‚Ì’e”Aƒ}ƒKƒWƒ““à‚ÌÅ‘å”
+		int ammunition = 110, maxAmmunition = 110, ammoClip = 25, maxAmmoClip = 25;
+		//•â[‚·‚é‚×‚«’e–ò‚Ì•Ï”‚Æ•â[‚·‚é’e–ò‚Ì•Ï”
+		int amountNeed = 0, ammoAvailable = 0;
+		float reroad_x = 500.0, reroad_y = 400.0;
+		float reroadtime = 3.0f;
+		int SubMachinGun_Rrroad = 0;
+	
+	};
+	SubMachineGunStatus sub_status;
+
+	struct AssaultRifleStatus {
+		int AssaultRifleImage = 0;
+		//Š’e–òAÅ‚Š’e–òAƒ}ƒKƒWƒ““à‚Ì’e”Aƒ}ƒKƒWƒ““à‚ÌÅ‘å”
+		int ammunition = 90, maxAmmunition = 90, ammoClip = 30, maxAmmoClip = 30;
+		//•â[‚·‚é‚×‚«’e–ò‚Ì•Ï”‚Æ•â[‚·‚é’e–ò‚Ì•Ï”
+		int amountNeed = 0, ammoAvailable = 0;
+		float reroad_x = 500.0, reroad_y = 400.0;
+		float reroadtime = 4.0f;
+		int AssaultRifle_Reroad = 0;
+	};
+	AssaultRifleStatus assault_status;
+	
+	
+	//•Ší‚Ì‰æ‘œ
 	int HandGunImage = 0;
 	int SubMachineGunImage = 0;
 	int AssaultRifleImage = 0;
-
+	int NormalWeaponImage = 0;
+	//e‚ÌƒŠƒ[ƒh‰¹
 	int HandGun_Reroad = 0;
+	int SubMachinGun_Reroad = 0;
+	int AssaultRifle_Reroad = 0;
+	int NormalWeapon_Reroad = 0;
 	//ƒ}ƒEƒXƒzƒC[ƒ‹‚Ì‰ñ“]—Ê•Ï”
 	int mousewheel = 0;
 	//Š’e–òAÅ‚Š’e–òAƒ}ƒKƒWƒ““à‚Ì’e”Aƒ}ƒKƒWƒ““à‚ÌÅ‘å”
@@ -28,11 +72,12 @@ public:
 	int debug = 0;
 	int Tama_0 = 0;
 	float reroad_x = 500.0, reroad_y = 400.0;
-	double debug_i = 0.f;
+	double normalreroad_time = 0;
+	float reroadtime = 0.f;
 	void Initialize();
 	void Update(float deltaTime);
 	void Render();
-
+	void SwitchWeapon(WeaponType weapontype);
 	bool HandGunFlag = false;
 	bool SubMachineGunFlah = false;
 	bool AssaultRifleFlag = false;
